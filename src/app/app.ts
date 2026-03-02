3446a11ddef0d33e0b28cfca4b4f0d8ff0b4a7dc
pumans <79007768+pumans@users.noreply.github.com>
2026-03-02 22:36:58 +0200

    Maket

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header';
+import { HeroComponent } from './components/hero/hero';
+import { MainContent } from './components/main-content/main-content';
+import { SidebarLeft } from './components/sidebar-left/sidebar-left';
+import { SidebarRight } from './components/sidebar-right/sidebar-right';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, HeroComponent, MainContent, SidebarLeft, SidebarRight, Footer],
+  templateUrl: `./app.html`,
+  styles: [
+    `
+      .container {
+        max-width: 1200px;
+        margin: 20px auto;
+        padding: 0 15px;
+      }
+    `,
+  ],
 })
 export class App {
-  protected readonly title = signal('client');
+  title = 'pravo-news';
 }
